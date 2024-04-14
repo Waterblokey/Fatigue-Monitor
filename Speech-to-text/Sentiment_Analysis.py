4fc1de650ee374076b91afcff7d7b6e02a0bab8c
Ryan Livengood <118576768+Waterblokey@users.noreply.github.com>
2024-04-14 00:59:48 -0700

    Simplified Code

--- Speech-to-text/Sentiment_Analysis.py
+++ Speech-to-text/Sentiment_Analysis.py
@@ -10,7 +10,6 @@ def print_result(annotations):
 
     sentiment = "Neutral"
 
-#    print("Overall Sentiment: score of", score, " with magnitude of", magnitude)
     if((score > 0.5) & (magnitude > 0.25)):
         sentiment = "Positive"
     elif((score < -0.5) & (magnitude > 0.25)):
