0c63318c06f216229720c19dd905d68a17e29c5f
Waterblokey <118576768+Waterblokey@users.noreply.github.com>
2024-04-13 18:47:52 -0700

    created sentiment analysis

--- Speech-to-text/STTTrial.py
+++ Speech-to-text/STTTrial.py
@@ -3,6 +3,6 @@
 import whisper
 
 model = whisper.load_model("base")
-result = model.transcribe("outro.mp4", language="en")
+result = model.transcribe("Speech-to-text/outro.mp4", language="en")
 print(result["text"])
 
---
+++ Speech-to-text/Sentiment_Analysis.py
@@ -0,0 +1,20 @@
+import argparse
+import google.cloud.language as language
+
+def print_result(annotations):
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    print("Overall Sentiment: score of", score, " with magnitude of", magnitude)
+    return score
+
+def analyze(text):
+    """Run a sentiment analysis request on text within a passed filename."""
+    client = language.LanguageServiceClient()
+
+    document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(request={'document': document})
+
+    return print_result(annotations)
+
+analyze("Speech-to-text/outro.mp4")
